095ed2fce1de483f3c2edce769ae650c36440252
Lindokuhle Mncwabe <Lindokuhle.Mncwabe@investec.co.za>
2024-04-17 22:40:31 +0200

    sln file and new dib file

--- 003-C#/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ 003-C#/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fddad41a6209a97f0b070d0702db10b70af7b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4094d314cab70968382ffae5582d5be8b2cf0d6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
